e5c622b6c39576fd178826c17af327dccbd6b44c
박도영 <chipoli410@gmail.com>
2026-03-10 09:42:43 +0900

    Updated Changes

--- week01/Coding Practice 1.py
+++ week01/Coding Practice 1.py
@@ -285,7 +285,13 @@ else:
 """# 4. 반복문"""
 
 #for 반복문2
-number
+number = 0
+for score in [90, 25, 67, 45, 93]:
+    number += 1
+
+    if score > 60:
+        print("%d번 학생은 합격입니다," % number)
+        
 
 
 
