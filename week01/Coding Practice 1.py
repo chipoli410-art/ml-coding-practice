6122c9525e7162cb3aa7cf0fa1b3a912d1e37ddd
박도영 <chipoli410@gmail.com>
2026-03-10 10:13:14 +0900

    Updated Changes

--- week01/Coding Practice 1.py
+++ week01/Coding Practice 1.py
@@ -409,6 +409,15 @@ datetime.now()
 f = open('example.txt', 'w')
 print(f)
 
+#파일 닫기
+f.close()
+
+#파일 쓰기
+f = open('example.txt', 'w')
+for i in range(1, 6):
+    data = '%d번째 줄입니다. \n' % i
+    f.write(data)
+
 
 
 
