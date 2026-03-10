6ad8e7abda6d293e5e289a6d480ef4dca77fd15d
박도영 <chipoli410@gmail.com>
2026-03-10 10:09:14 +0900

    Updated Changes

--- week01/Coding Practice 1.py
+++ week01/Coding Practice 1.py
@@ -397,7 +397,12 @@ print(sorted('Python'))
 ## Request('http://www.sunmoon.ac.kr')   # 오류 발생
 
 import urllib.request
-urllib.request.Request()
+urllib.request.Request('http://www.sunmoon.ac.kr')
+
+import pandas
+pandas,DataFrame(
+    
+)
 
 
 
