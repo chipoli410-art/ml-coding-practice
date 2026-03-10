ce481154a74fe20883f458165995c60ca881a391
박도영 <chipoli410@gmail.com>
2026-03-10 09:29:13 +0900

    Updated Changes

--- week01/Coding Practice 1.py
+++ week01/Coding Practice 1.py
@@ -211,6 +211,15 @@ print(list(dic.values()))
 print(dic.items())
 
 for key, value in dic.items():
+    print(key + ":" + value)
+
+# 원소 삭제
+dic.clear()
+print(dic)
+
+#집합 만들기
+s1 = {1, 2, 'a', 5}
+s2
 
 
 
