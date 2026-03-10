ac5727613591318762bf6f8d226bcdc28666892f
박도영 <chipoli410@gmail.com>
2026-03-10 10:15:44 +0900

    Updated Changes

--- week01/Coding Practice 1.py
+++ week01/Coding Practice 1.py
@@ -420,7 +420,10 @@ for i in range(1, 6):
 f.close()
 
 #v 파일에 내용 추가하기
-f = open('example.txt')
+f = open('example.txt', 'w')
+for i in range(6, 11):
+    data = '%d번째 줄 추가입니다. \n' % i
+    f,wr
 
 
 
