8b84d8e3dd93ba94e1c326c948cdd5bfade5cb8a
박도영 <chipoli410@gmail.com>
2026-03-17 13:18:05 +0900

    Updated Changes

--- week02/pandas_numpy.py
+++ week02/pandas_numpy.py
@@ -71,7 +71,11 @@ data = {
     'is_spam': [False, False, True, False, False, True, False, False],
     'has_image': [True,False, True, True, False, False, True, True]
 }
+df = pd.DataFrame(data)
+print(df.head())
 
+# 필터링 조건 설정
+condition =
 
 
 
