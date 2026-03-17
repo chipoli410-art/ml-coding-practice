e59735a081fa42e6904a73a7342d0f2b5d6847cd
박도영 <chipoli410@gmail.com>
2026-03-17 13:17:08 +0900

    Updated Changes

--- week02/pandas_numpy.py
+++ week02/pandas_numpy.py
@@ -67,8 +67,11 @@ print(more2015_or_tv.head())
 data = {
     'name': ['Alice', 'Bob', 'Charlie', 'David', 'Eve', 'Feank', 'Grace', 'Hannah'],
     'comment_length': [150, 200, 50, 300, 120, 180, 75, 160],
-    ''
+    'likes': [25, 30, 10, 45, 20, 35, 5, 28],
+    'is_spam': [False, False, True, False, False, True, False, False],
+    'has_image': [True,False, True, True, False, False, True, True]
 }
+
 
 
 
