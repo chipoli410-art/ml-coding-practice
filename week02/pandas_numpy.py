2c729b0eccd92537138ead6cce6fcb937e0eece6
박도영 <chipoli410@gmail.com>
2026-03-17 13:58:05 +0900

    Updated Changes

--- week02/pandas_numpy.py
+++ week02/pandas_numpy.py
@@ -197,4 +197,6 @@ print(np.arange(8).reshape(2, 4) + 10)
 a = np.arange(8).reshape(2, 4) **2
 print(a)
 
-print(a.sum())
+print(a.sum())     # 모든 요소의 합
+print(a.mean())    # 모든 요소의 평균
+print(a.mean(axis = 0))
