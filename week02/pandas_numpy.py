681341b8a405b29abe297a58577f3836bba1d836
박도영 <chipoli410@gmail.com>
2026-03-17 13:25:05 +0900

    Updated Changes

--- week02/pandas_numpy.py
+++ week02/pandas_numpy.py
@@ -94,8 +94,10 @@ print(sample_df.set_index('var_1'))
 
 
 print(sample_df.describe())
-print(sample_df.std)
+print(sample_df.std())
+print(sample_df.agg(['count','mean', 'std', 'min', 'max']))
 
+file_url =
 
 
 
