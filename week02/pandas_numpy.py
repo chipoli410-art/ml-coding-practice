f8527ef8098cf5968b4e73a3d575edf31241eebd
박도영 <chipoli410@gmail.com>
2026-03-17 13:06:35 +0900

    Updated Changes

--- week02/pandas_numpy.py
+++ week02/pandas_numpy.py
@@ -53,7 +53,10 @@ more 2015 = netfix[netfix['release_year'] > 2015]
 print(more2015.head(10))
 
 print(~(netfix['release_year'] > 2015))
+less2015 = netflix[~(netflix['release_year'] > 2015)]
+print(less2015.head())
 
+print((netflix['release_year'] > 2015
 
 
 
