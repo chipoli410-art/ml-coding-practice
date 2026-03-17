488005de471f6f0ea6dac5beb8cebe5f3c875efd
박도영 <chipoli410@gmail.com>
2026-03-17 13:47:06 +0900

    Updated Changes

--- week02/pandas_numpy.py
+++ week02/pandas_numpy.py
@@ -161,4 +161,5 @@ print(pd.DataFrame(sample_np, columns= sample_df.columns))      # 새 프레임�
 print(sample_np)
 print(sample_np[0])
 print(sample_np[0,2])
-print(s)
+print(sample_np[0:3,2:4])
+print(sample_np[:,2])
