e0181d48d34f2c8f6d406a5f2b65f7caa03b6d8d
박도영 <chipoli410@gmail.com>
2026-03-17 13:20:05 +0900

    Updated Changes

--- week02/pandas_numpy.py
+++ week02/pandas_numpy.py
@@ -77,7 +77,9 @@ print(df.head())
 # 필터링 조건 설정
 condition = (
     (df['commment_length'] >= 100) &       # 댓글 길이 100자 이상
-    (df['likes'])&                 # 좋아요 20개 이상
+    (df['likes'] >= 20)&                 # 좋아요 20개 이상
+    (~df['is_spam']) &                    # 스팸 댓글이 아니어야 함
+
 
 
 )
