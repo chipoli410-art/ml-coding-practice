575ee3bad45b644f7fbb92f96f68488d154ef52e
박도영 <chipoli410@gmail.com>
2026-03-23 11:49:28 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -29,4 +29,6 @@ plt.xlabel('Pclass')
 plt.ylabel('Survival Rate')
 plt.xticks([1, 2, 3])
 plt.grid(True)
-print.savefig('Figure01.png')
+print.savefig('Figure01.png')         # 결과를 그림파일로 저장
+plt.close()                         # 다음 Plot을 새로 그리기 위해 plt 닫기
+
