0c2b6f2cdf9117e861d8ee65b55204abf03b1ed7
박도영 <chipoli410@gmail.com>
2026-03-24 09:56:42 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -93,5 +93,5 @@ alpha=0.7)
 plt.title('Age and Fare Relationship with Survival on the Titanic')
 plt.xlabel('Age')
 plt.ylabel('Fare')
-plt.;=legend(handles=scatter.legend_elements()[0], title= 'Survived',
+plt.=legend(handles=scatter.legend_elements()[0], title= 'Survived',
            labels=['Not Survived', 'Survived'], loc='upper right')
