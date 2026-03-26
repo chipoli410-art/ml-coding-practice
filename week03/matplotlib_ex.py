5fd9c29a6cb2a710c80cb0bdf67c02a29a25f779
박도영 <chipoli410@gmail.com>
2026-03-26 09:11:10 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -230,3 +230,9 @@ plt.xticks([1, 2, 3], ['1st Class', '2nd Class', '3rd Class'])
 plt.legend(violin_plot['bodies'], ['1st Class', '2nd Class', '3rd Class'],
         title='Pclass', loc="upper right")
 plt.savefig('Figure10.png')
+plt.close()
+
+## **에러 바 : 요금의 평균과 표준편차 표현하기**
+
+#각 부모와 자녀의 수에 대한 요금의 평균과 표준 편차 계산
+fare_means
