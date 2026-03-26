bc954dc99943f742fe9ec98e45a0e58c521a143e
박도영 <chipoli410@gmail.com>
2026-03-26 09:52:11 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -284,7 +284,7 @@ plt.plot(x, y1, '-o', color='indigo', markersize=7, linewidth=3, alpha=0.7,
 label='Not Survived')
 plt.xlabel('Parch')
 plt.ylabel('Not Survived Count', color='indigo')
-plt.tick
+plt.tick_params(axis='y', labelcolor='indigo')
 
 
 
