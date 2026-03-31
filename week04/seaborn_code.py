0fea10d6e0bd71aa1964286853aa4901b7701bf3
박도영 <chipoli410@gmail.com>
2026-03-31 09:39:30 +0900

    Updated Changes

--- week04/seaborn_code.py
+++ week04/seaborn_code.py
@@ -14,4 +14,8 @@ tips.info()
 import matplotlib.pyplot as plt
 
 # figure에 2개의 서브 플롯을 생성
-fig = plt.f
+fig = plt.figure(figsize=(15, 5))
+ax1 = fig.add_subplot(1, 2, 1)
+ax2 = fig.add_subplot(1, 2 ,2)
+
+
