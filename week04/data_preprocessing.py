b0531d105439eb390565cb03ab76f1ee8ca73422
박도영 <chipoli410@gmail.com>
2026-03-31 14:58:19 +0900

    Updated Changes

--- week04/data_preprocessing.py
+++ week04/data_preprocessing.py
@@ -46,5 +46,9 @@ housing_labels = strat_train_set["median_house_value"].copy()
 null_rows_idx = housing.isnull().any(axis=1)
 housing.loc[null_rows_idx].head()
 
+from sklearn.impute import SimpleImputer
 
+imputer = SimpleImputer(strategy="median")
+
+#수치형 특성만 추출
 
