e64d171d71a338b85c098ca53c7e951da199de97
박도영 <chipoli410@gmail.com>
2026-03-31 13:48:48 +0900

    Updated Changes

--- week04/seaborn_code.py
+++ week04/seaborn_code.py
@@ -73,5 +73,10 @@ plt.savefig('./week04/Seaborn_Figure04.jpg')
 # **조인트 그래프**
 # jointplot( ) 그리기
 sns.jointplot(x='size', y='tip', data=tips, kind='scatter')
+plt.savefig('./week04/Seaborn_Figure05.jpg')
+
+# **관계 그래프**
+# pairplot( ) 그리기
+
 
 
