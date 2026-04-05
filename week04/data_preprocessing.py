053f14160319bec3a0419cab6124b1fd50863123
박도영 <chipoli410@gmail.com>
2026-04-05 14:08:07 +0900

    Updated Changes

--- week04/data_preprocessing.py
+++ week04/data_preprocessing.py
@@ -35,3 +35,11 @@ null_rows_idx = housing.isnull().any(axis=1)
 housing.loc[null_rows_idx].head()
 
 from sklearn.impute import SimpleImputer
+
+imputer = SimpleImputer(strategy="median")
+
+# 수치형 특성만 추출
+housing_num = housing.select_dtypes(include=[np.number])
+housing_num.head()
+
+imputer.fit(housing_num)
