10725831488b32cc33d9d8802f8ff0c29a70ea76
박도영 <chipoli410@gmail.com>
2026-04-14 14:23:39 +0900

    Updated Changes

--- week06/tourism.py
+++ week06/tourism.py
@@ -63,6 +63,11 @@ def getTourismStatsItem(yyyymm, nat_cd, ed_cd):
     parameters += "&NAT_CD=" + nat_cd
     parameters += "&ED_CD=" + ed_cd
 
+    url = service_url + parameters
+
+    response = urllib.request.urlopen(url) 	                    #[CODE 1]
+    
+
 
 
 
