f74f522b8fdfe571ff5b5e6124cd40cb15cbe8fc
박도영 <chipoli410@gmail.com>
2026-04-14 13:44:38 +0900

    Updated Changes

--- week06/tourism.py
+++ week06/tourism.py
@@ -10,4 +10,8 @@ service_key = "27abaeaae1e3e2aaf79e45f5312beb77036874ff34a49a55f8a5cc202fdd2efd"
 
 def main():
     jsonResult =[]
+    result = []
+
+    print("<< 국내 입국한 외국인의 통계 데이터를 수집 합시다. >>")
+    nat_cd = input('국가 코드를 입력하세요(중국:112 / 일본: 130 / 미국: 275):')
     
