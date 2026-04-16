3df9f233b49312761424797b0ecba50164547c8e
박도영 <chipoli410@gmail.com>
2026-04-16 09:12:56 +0900

    Updated Changes

--- week06/naverapi.py
+++ week06/naverapi.py
@@ -17,7 +17,7 @@ def main():
     jsonResponse = getNaverSearch(node, srch_text, 1, 100)      # [CODE 2]
     total = jsonResponse['total']
 
-    while ((jsonResponse != None) and (jsonResponse['display']! = 0)):
+    while ((jsonResponse != None) and (jsonResponse['display'] != 0)):
         for post in jsonResponse['items']:
             cnt += 1
             getPostData(post, jsonResult, cnt)                # [CODE 3]
