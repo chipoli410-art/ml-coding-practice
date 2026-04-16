ad306ff9a038dd31dd81ef371adeef9ce89de4a0
박도영 <chipoli410@gmail.com>
2026-04-16 10:12:58 +0900

    Updated Changes

--- week06/naverapi.py
+++ week06/naverapi.py
@@ -9,12 +9,12 @@ client_secret = 'c9L3L_G6HK'
 def main():
 
     node = 'news'                                            # 크롤링할 대상
-    srch_text = input('검색어를 입력하세요:')
+    srcText = input('검색어를 입력하세요:')
 
     cnt = 0
     jsonResult = []
 
-    jsonResponse = getNaverSearch(node, srch_text, 1, 100)      # [CODE 2]
+    jsonResponse = getNaverSearch(node, srcText, 1, 100)      # [CODE 2]
     total = jsonResponse['total']
 
     while ((jsonResponse != None) and (jsonResponse['display'] != 0)):
