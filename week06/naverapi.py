88c216eaba99e4501b1a1c2cc7089d31903326a1
박도영 <chipoli410@gmail.com>
2026-04-16 09:55:28 +0900

    Updated Changes

--- week06/naverapi.py
+++ week06/naverapi.py
@@ -59,6 +59,12 @@ def getRequestUrl(url):
         response = urllib.request.urlopen(req)
         if response.getcode() == 200:
             print("[%s] Url Request Success" % datetime.datetime.now())
+            return response.read().decode('utf-8')
+    except Exception as e:
+        print(e)
+        
+
+
 
 
     
