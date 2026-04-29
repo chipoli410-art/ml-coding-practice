bac4a9c106ba9b7f62d1e3df10198600f4394468
박도영 <chipoli410@gmail.com>
2026-04-29 10:21:40 +0900

    Updated Changes

--- week07/classification.py
+++ week07/classification.py
@@ -54,4 +54,5 @@ cross_val_score(sgd_clf, X_train, y_test_5, cv=3, scoring="accuracy")
 from sklearn.dummy import DummyClassifier
 
 dummy_clf = DummyClassifier
-dummy_clf.fit(X_train, y)
+dummy_clf.fit(X_train, y_test_5)
+print(any(dummy_clf.predict(X_train)))
