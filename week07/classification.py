4b488fc784959fed0e784f1d31025b343bcca13a
박도영 <chipoli410@gmail.com>
2026-04-29 10:38:40 +0900

    Updated Changes

--- week07/classification.py
+++ week07/classification.py
@@ -88,3 +88,6 @@ for sklearn.metrics import roc_auc_score
     
 y_scores = cross_val_predict(sgd_clf, X_train, y_train_5, cv=3, method="decision_function")
 roc_auc_score(y_train_5, y_scores)
+
+#다음은 실행 하는데 몇분이 걸릴 수 있음
+from sklearn.ensemble import RandomForestClassifier
