d05f695d9d813a8335cdc180892750fc9387d156
박도영 <chipoli410@gmail.com>
2026-04-29 10:31:40 +0900

    Updated Changes

--- week07/classification.py
+++ week07/classification.py
@@ -73,7 +73,7 @@ y_train_perfect_predictions = y_test_5 #완벽한 분류기일 경우
 print(confusion_matrix(y_train_5, y_train_perfect_predictions))
 
 # 정밀도와 재현율
-from sklearn
+from sklearn.metrics import precision
 
 
 
