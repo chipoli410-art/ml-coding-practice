7676650e74aba673dcfdd30dfa960ab796342a34
박도영 <chipoli410@gmail.com>
2026-05-04 23:33:17 +0900

    Updated Changes

--- data_analysis/netflix_project.py
+++ data_analysis/netflix_project.py
@@ -195,3 +195,9 @@ from wordcloud import WordCloud
 from PIL import Image
 
 plt.figure(figsize=(15, 5))
+
+# wordcolud에서 작동할 수 있도록 데이터프레임을 list로 1차 변환시키고 str(문자열)로 2차 변환
+text = str(list(netflix['description']))
+
+# 워드 클라우드 색상맵 만들기
+cmap = plt.matplotlib.colors.LinearSegmentedColormap.from_list('', ['#221f1f','#b20710'])
