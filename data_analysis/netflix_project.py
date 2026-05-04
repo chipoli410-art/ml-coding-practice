5bfea21783acbd984f729ab30b2edfceac67e8e7
박도영 <chipoli410@gmail.com>
2026-05-04 23:40:47 +0900

    Updated Changes

--- data_analysis/netflix_project.py
+++ data_analysis/netflix_project.py
@@ -54,8 +54,8 @@ netflix['age_group'] = netflix['rating']
 # 시청 등급 코드를 더 이해하기 쉬운 표현으로 매핑할 딕셔너리 정의(key, value 선언)
 age_group_dic = {
     'G': 'All',
-    'TV-G': 'ALL',
-    'TV-Y': 'ALL',
+    'TV-G': 'All',
+    'TV-Y': 'All',
     'PG': 'Older Kids',
     'TV-Y7': 'Older Kids',
     'TV-PG': 'Older Kids',
