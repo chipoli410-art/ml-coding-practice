c645dfb392bf8118798f750f05762fcef92417ea
박도영 <chipoli410@gmail.com>
2026-05-04 23:09:14 +0900

    Updated Changes

--- data_analysis/netflix_project.py
+++ data_analysis/netflix_project.py
@@ -108,5 +108,9 @@ plt.suptitle('Movie & TV Show distribution', fontfamily='serif', fontsize=15, fo
 plt.title('We see more movies than TV shows on Netflix.', fontfamily='serif', fontsize=12)
 plt.show()
 
+netflix.head(3)
 
+# 넷플릭스 데이터셋의 장르별 등장 횟수 계산
+genres = netflix['listed_in'].str.split(', ', expand=True).stack().value_counts()
+genres
 
