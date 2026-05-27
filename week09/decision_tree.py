31fc0f5220e059dd992b32d1a3f8d02636a59da0
박도영 <chipoli410@gmail.com>
2026-05-27 10:47:51 +0900

    Updated Changes

--- week09/decision_tree.py
+++ week09/decision_tree.py
@@ -52,6 +52,20 @@ X_quad =np.random.rand(200, 1) - 0.5  # 간단한 랜덤한 입력 특성
 y_quad = X_quad ** 2 + 0.025 * np.random.randn(200,1)
 
 tree_reg = DecisionTreeRegressor(max_depth=2, random_state=42)
+tree_reg.fit(X_quad, y_quad)
+
+export_graphviz(
+    tree_reg,
+    out_file="regression_tree.dot",
+    feature_names=["x1"],
+    rounded=True,
+    filled=True
+)
+Source.from_file("regression_tree.dot")
+
+"""#축 방향에 대한 민감성"""
+
+
 
 
 
