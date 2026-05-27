6b6eed37f3956a1313f0b65a2a3bef85ce4cc1ed
박도영 <chipoli410@gmail.com>
2026-05-27 09:40:45 +0900

    Updated Changes

--- week09/decision_tree.py
+++ week09/decision_tree.py
@@ -3,3 +3,7 @@ from sklearn.tree import DecisionTreeClassifier
 
 iris = load_iris(as_frame=True)
 X = iris.data[["petal length (cm)", "peral width (cm)"]].values
+y_iris = iris.target
+
+tree_clf = DecisionTreeClassifier(max_depth=2, random_state=42)
+tree_clf.
