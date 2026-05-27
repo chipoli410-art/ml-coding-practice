b5ccb4c5a5979faf30588be1ce22ae497e0e5a50
박도영 <chipoli410@gmail.com>
2026-05-27 10:59:52 +0900

    Updated Changes

--- week09/decision_tree.py
+++ week09/decision_tree.py
@@ -88,7 +88,8 @@ X_iris_pca_all = np.c_[z0s.ravel(), z1s.ravel()]
 y_pred = tree_clf_pca.predict(X_iris_pca_all).reshape(z0s.shape)
 
 plt.contourf(z0s, z1s, y_pred, alpha=0.3, cmap=custom_cmap)
-for idx,
+for idx, (name, style) in enumerate(zip(iris.target_names, ["o", "s", "^"])):
+    plt.scatter(X_iris_rotated[y_iris == idx, 0], X_iris_rotated[y_iris == idx, 1], marker=style, label=name)
 
 
 
