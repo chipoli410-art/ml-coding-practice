e77f7a93b7987268c7ab817fce716f5ccf811e59
박도영 <chipoli410@gmail.com>
2026-05-27 11:07:52 +0900

    Updated Changes

--- week09/decision_tree.py
+++ week09/decision_tree.py
@@ -80,7 +80,7 @@ from matplotlib.colors import ListedColormap
 
 custom_cmap = ListedColormap(["#fafab0", "#9898ff", "#a0faa0"])
 
-plt.fifure(figsize=(8,4))
+plt.figure(figsize=(8,4))
 
 axes = [-2.2, 2.4, -0.6, 0.7]
 z0s, z1s = np.meshgrid(np.linspace(axes[0], axes[1], 100), np.linspace(axes[2], axes[3], 100))
