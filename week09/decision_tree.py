8fe2a3b3a7e34672568f5593251f9abb3b4ae42b
박도영 <chipoli410@gmail.com>
2026-05-27 10:38:21 +0900

    Updated Changes

--- week09/decision_tree.py
+++ week09/decision_tree.py
@@ -37,7 +37,8 @@ tree_clf2 = DecisionTreeClassifier(min_samples_leaf=5, random_state=42)
 tree_clf1.fit(X_moons, y_moons)
 tree_clf2.fit(X_moons, y_moons)
 
-X_moons_test
+X_moons_test, y_moonstest = make_moons(n_samples= 1000, noise=0.2, random_state=43)
+
 
 
 
