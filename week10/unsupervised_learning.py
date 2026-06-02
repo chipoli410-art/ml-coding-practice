26ec58f2eb9ea6ae07cfce18231a17a008adb717
박도영 <chipoli410@gmail.com>
2026-06-02 10:31:16 +0900

    Updated Changes

--- week10/unsupervised_learning.py
+++ week10/unsupervised_learning.py
@@ -127,7 +127,7 @@ print(dbscan.labels_[:10])
 print(dbscan.core_sample_indices_[:10])
 print(dbscan.components_)
 
-def plot_dbscan(dbscan, X, siae, show_xlabels=True, show_ylabels=True):
+def plot_dbscan(dbscan, X, size, show_xlabels=True, show_ylabels=True):
     core_mask = np.zeros_like(dbscan.labels_, dtype=bool)
     core_mask[dbscan.core_sample_indices_] = True
     anomalies_mask = dbscan.labels_ == -1
@@ -138,3 +138,4 @@ def plot_dbscan(dbscan, X, siae, show_xlabels=True, show_ylabels=True):
     non_cores = X[non_core_mask]
 
     plt.scatter(cores[:,0], cores[:,1], c=dbscan.labels_[core_mask], marker='o', s=size, cmap= "Paired")
+    pl
