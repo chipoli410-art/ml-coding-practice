47ae509177ed4f5430872e11336a11e99d07d385
박도영 <chipoli410@gmail.com>
2026-06-02 09:34:46 +0900

    Updated Changes

--- week10/unsupervised_learning.py
+++ week10/unsupervised_learning.py
@@ -21,3 +21,8 @@ X, y = make_blobs(n_samples=2000, centers=blob_centers, cluster_std=blob_std, ra
 
 k = 5
 kmeans =KMeans(n_clusters=k, n_init=10, random_state=42)
+y_pred = kmeans.fit_predict(X)
+
+def plot_clusters(X, y=None):
+    plt.scatter()
+
